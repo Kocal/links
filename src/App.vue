a57c64758a0f286714e5dd2c116a2d1e9d7b0d55
Hugo Alliaume <halliaume@wamiz.com>
2021-09-06 22:19:32 +0200

    refactor: make hover bg more maintenable

--- src/App.vue
+++ src/App.vue
@@ -11,19 +11,19 @@ import Link from '@/components/Link.vue';
     </header>
 
     <main class="flex flex-col space-y-5">
-      <Link url="https://github.com/Kocal" class="bg-[#24292f] hover:bg-[#2f363d] text-white">
+      <Link url="https://github.com/Kocal" class="bg-[#24292f] filter hover:brightness-110 text-white">
         <FontAwesomeIcon :icon="['fab', 'github']" />
         GitHub
       </Link>
-      <Link url="https://twitter.com/Kocal_" class="bg-[#00acee] hover:bg-[#09bbff] text-white">
+      <Link url="https://twitter.com/Kocal_" class="bg-[#00acee] filter hover:brightness-110 text-white">
         <FontAwesomeIcon :icon="['fab', 'twitter']" />
         Twitter (1)
       </Link>
-      <Link url="https://twitter.com/HugoAlliaume" class="bg-[#00acee] hover:bg-[#09bbff] text-white">
+      <Link url="https://twitter.com/HugoAlliaume" class="bg-[#00acee] filter hover:brightness-110 text-white">
         <FontAwesomeIcon :icon="['fab', 'twitter']" />
         Twitter (2)
       </Link>
-      <Link url="https://www.instagram.com/thekocal" class="bg-[#dd2a7b] hover:bg-[#e14089] text-white">
+      <Link url="https://www.instagram.com/thekocal" class="bg-[#dd2a7b] filter hover:brightness-110 text-white">
         <FontAwesomeIcon :icon="['fab', 'instagram']" />
         Instagram
       </Link>
